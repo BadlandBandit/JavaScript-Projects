aeec5b172da7378e746d319663a1664c1684dbd3
BadlandBandit <rabbitcrs@gmail.com>
2026-01-27 10:27:15 -0700

    update

---
+++ Basic-JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    var sentence = "I am learning"; // creating a variable with a string
+    sentence += " a lot from this course!"; // using the += operator to add to the variable
+    document.getElementById("Concatenate").innerHTML = sentence; // output the result to an HTML element
+}
+
