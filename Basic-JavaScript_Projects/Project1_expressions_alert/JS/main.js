364f0cbe79947f5a89ffb952a3f854cfa402c2dc
BadlandBandit <rabbitcrs@gmail.com>
2026-01-27 09:36:19 -0700

    update

--- Basic-JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -3,21 +3,16 @@
 // window.alert method
 window.alert("Hello, world!");
 
-// document.write method (replaced with DOM manipulation)
-const para1 = document.createElement("p");
-para1.textContent = "For the last time, hello, world!";
-document.body.appendChild(para1);
+// document.write method
+document.write("For the last time, hello, world!");
 
 // creating two variables
 var Sent1 = "This is the beginning of the string";
 var Sent2 = " and this is the end of the string";
 
-// displaying the variables using DOM manipulation
-const para2 = document.createElement("p");
-para2.textContent = Sent1 + Sent2;
-document.body.appendChild(para2);
+// displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
 
-// an expression (output using DOM manipulation)
-const para3 = document.createElement("p");
-para3.textContent = 33 * 84;
-document.body.appendChild(para3);
+// an expression
+document.write(33 * 84);
+
