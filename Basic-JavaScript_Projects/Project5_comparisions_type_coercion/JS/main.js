2f08ab336159339da4f8d64096b337183de3af0a
BadlandBandit <rabbitcrs@gmail.com>
2026-01-29 10:00:27 -0700

    update

---
+++ Basic-JavaScript_Projects/Project5_comparisions_type_coercion/JS/main.js
@@ -0,0 +1,33 @@
+//variable with Boolean data type
+var x = true;
+
+//printing the dta type of the variable x
+document.write(typeof x);
+document.write("<br>");
+
+//an expresion combining a string and a number
+document.write("10" + 10);
+document.write("<br>");
+
+//utilizing the == operator
+document.write(5 == 5);
+document.write("<br>");
+
+//utilizing the === operator
+x = 1;
+y = 1;
+document.write(x === y);
+document.write("<br>");
+
+//utilizing the > and && operators
+document.write(10 > 5 && 11 > 6);
+document.write("<br>");
+
+//utilizing the < and || operators
+document.write(10 < 5 || 11 > 6);
+document.write("<br>");
+
+//function utilizing the ! operator
+function not_function() {
+    document.getElementById("not").innerHTML = !(1 > 2);
+}
